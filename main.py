d372210968308d9c54bd21efa16433031be3649d
Muhammad Nurdin <62412936+muhanur@users.noreply.github.com>
2024-05-29 20:00:10 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import warnings
+import sys
+warnings.filterwarnings('ignore')
+
+import numpy as np, pandas as pd
+from numpy import inf
+
+Oi = np.array([100, 120, 140])
+Dj = np.array([70, 150, 130])
+W_data = np.array([[0, 7, 2],[7, 0, 6],[2, 6, 0]])
+
+Oi = np.round(pd.read_excel('./data/supply.xlsx').T.values[0], decimals=4)
+Dj = pd.read_excel('./data/demand.xlsx').T.values[0]
+W_data = pd.read_excel('./data/weight.xlsx').to_numpy()
+
+filepath = "./data/result.csv"
+i = 1
+
+# Write file
+def write_file(filepath, data):
+    df = pd.DataFrame(data)
+    df.to_csv(filepath, index=False)
+
+# Create first temp data
+W_temp = (Dj * Oi[np.newaxis].T) / W_data
+W_temp[W_temp == inf] = 0 #Change inf to 0
+type = 1
+num_of_iteration = 10000
+
+while i < num_of_iteration:
+    print("Iteration {}".format(i + 1))
+    if type == 0:
+        Dd = W_temp.sum(axis=type)
+        if (Dd==Dj).all() == False:
+            Bj = Dj / Dd
+            W_temp = W_temp * Bj
+            type = 1
+        else:
+            write_file(filepath, W_temp)
+            print("Iteration Finish")
+            break
+    else:
+        Dd = W_temp.sum(axis=type)
+        if (Dd==Oi).all() == False:
+            Ai = Oi / Dd
+            W_temp = W_temp * Ai[np.newaxis].T
+            type = 0
+        else:
+            write_file(filepath, W_temp)
+            print("Iteration Finish")
+            break
+
+    i += 1
+
+    if i == num_of_iteration:
+        write_file(filepath, W_temp)
